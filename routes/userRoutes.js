eaa5e44174abb2b38477b7d4d53d1462bcf39f47
ramson12 <faria12.ransom@gmail.com>
2020-03-11 18:58:31 +0530

    missing packages added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const multer = require('multer');
+//const multer = require('multer');
 const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
